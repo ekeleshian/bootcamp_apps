fe167d46a97bc0ecb76adcf28f8e94faba5a9ea1
elizabeth <ekeleshian@gmail.com>
2018-07-31 14:58:41 -0700

    finished first question except for one brownie point

--- q1.py
+++ q1.py
@@ -36,7 +36,8 @@ This question should be written in Python. '''
 
 import sys
 import re
-
+import pandas as pd
+# from nltk import ngrams, FreqDist
 # file = open('sample.txt', 'r')
 
 
@@ -47,7 +48,7 @@ def word_count(file):
 	for l in file:
 		for i in p.findall(l):
 			words.append(i)
-	return len(words), len(set(words))
+	return len(words), len(set(words)), words
 
 def sentence_count(file):
 	sentences = []
@@ -74,15 +75,44 @@ def average_sentence_length(sentences):
 		counter = 0
 	return float(sum(number_of_words)/len(sentences))
 
+# def common_phrase(sentences):
+# 	all_counts = dict()
+# 	for i in sentences:
+# 		for size in 3, 4, 5:
+# 			all_counts[size] = FreqDist(ngrams(i, size))
+# 	return all_counts
 
-with open('sample.txt', 'r') as f:
+def list_of_words(unique_words, sentences):
+	words = []
+	dictionary = {}
+	sorted_words = []
+	reg_ex = r"[\s\.\?!]"
+	p = re.compile(reg_ex)
+	for s in sentences:
+		for i in p.split(s):
+			words.append(i)
+	for u in unique_words:
+		dictionary[u] = 0
+	for w in words:
+		if w in dictionary:
+			dictionary[w] += 1
+	sorted_by_value= sorted(dictionary.items(), key= lambda kv: kv[1], reverse = True)
+	for i in sorted_by_value:
+		sorted_words.append(i[0])
+	return sorted_words
+
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
 	sentences, sentence_count = sentence_count(f)
-with open('sample.txt', 'r') as f:
-	word_count, unique_word_count = word_count(f)
+with open(filename, 'r') as f:
+	word_count, unique_word_count, words = word_count(f)
 
 print('Total word count:  {}\n'.format(word_count) + 
 	'Unique words:  {}\n'.format(unique_word_count) + 
 'Sentences:  {}\n'.format(sentence_count) +
-'Average sentence length:  {}'.format(average_sentence_length(sentences)))
+'Average sentence length:  {}\n'.format(average_sentence_length(sentences)) + 
+"List of common words from most to least frequent:  {}\n".format(list_of_words(set(words), sentences)))
 
 
